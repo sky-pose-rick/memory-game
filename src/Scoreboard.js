291afd4596b504ddc0bc8de819f4e01d1aa80915
Richard Cranwell <rickycranwell@gmail.com>
2021-12-24 17:44:13 -0600

    add functionality to scoreboard

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,12 +1,25 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Scoreboard() {
+function Scoreboard(props) {
+  let { score } = props;
+  if (!score) { score = 0; }
+
+  const [highScore, setHighScore] = useState(score);
+
+  useEffect(() => {
+    if (score > highScore) { setHighScore(score); }
+  }, [score]);
+
   return (
     <div className="Scoreboard">
       scoreboard
-      <p>High Score: </p>
-      <p>Current Score: </p>
+      <p>
+        {`High Score: ${highScore}`}
+      </p>
+      <p>
+        {`Current Score: ${score}`}
+      </p>
     </div>
   );
 }
--- src/Scoreboard.test.js
+++ src/Scoreboard.test.js
@@ -5,5 +5,21 @@ import Scoreboard from './Scoreboard';
 
 it('Scoreboard renders', () => {
   render(<Scoreboard />);
-  expect(screen.getByText(/scoreboard/)).toBeInTheDocument();
+  expect(screen.getByText(/Current Score: 0/)).toBeInTheDocument();
+});
+
+it('Scoreboard displays current score', () => {
+  render(<Scoreboard score={5} />);
+  expect(screen.getByText(/Current Score: 5/)).toBeInTheDocument();
+});
+
+it('Scoreboard tracks high score', () => {
+  const { rerender } = render(<Scoreboard score={0} />);
+  expect(screen.getByText(/High Score: 0/)).toBeInTheDocument();
+  rerender(<Scoreboard score={4} />);
+  expect(screen.getByText(/High Score: 4/)).toBeInTheDocument();
+  rerender(<Scoreboard score={2} />);
+  expect(screen.getByText(/High Score: 4/)).toBeInTheDocument();
+  rerender(<Scoreboard score={7} />);
+  expect(screen.getByText(/High Score: 7/)).toBeInTheDocument();
 });
