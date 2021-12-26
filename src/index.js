c061e8ebd0882437f30b6be0180c83ca0c3b1fd3
Richard Cranwell <rickycranwell@gmail.com>
2021-12-26 16:17:59 -0600

    let app render

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const deck = [{
+  src: 'zebra.png',
+  text: 'zebra',
+  alt: 'zebra',
+}, {
+  src: 'bunny.png',
+  text: 'bunny',
+  alt: 'bunny',
+}, {
+  src: 'frog.png',
+  text: 'frog',
+  alt: 'frog',
+}];
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <App data={deck} demo />,
+  document.getElementById('root'),
 );
