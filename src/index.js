f79305021210bad8bab41c867ecdfacd7b63ea7a
Richard Cranwell <rickycranwell@gmail.com>
2021-12-27 15:51:13 -0600

    move components to component folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import cardData from './cards.json';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <App data={cardData.body} />,
